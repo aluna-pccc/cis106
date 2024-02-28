cc1009c6151858c32f4b0ffdcc35bcc89c9b4cc6
aluna1740@students.pccc.edu <aluna1740@students.pccc.edu>
2024-02-28 18:49:26 -0500

    bash practice

---
+++ scripts/script_env_var.sh
@@ -0,0 +1,34 @@
+# Author: Alberto Luna
+# Date: 02//2024
+# Description: 
+
+
+#!/bin/bash
+echo "This is the current user: " $USER
+
+# User devine variable
+name="AL"
+age="43"
+h="5.7"
+w="163 lbs"
+
+echo "The name is: $name"
+echo "$name is $age years old"
+echo "He has $w"
+echo "and is $h inches tall"
+
+# Using variables with commands: 
+
+# list all the files in the users' Downloads Folder
+
+ls $HOME/Downloads
+
+# list all the files in the home folder including hidden
+
+ls -al $HOME
+
+#using Variables in path
+
+ls -l /home/$USER/Pictures
+
+ls -al /home/$USER/Pictures
